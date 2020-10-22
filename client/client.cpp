fa0ba6a892049391f70f783d5c0d63a4cf2c602d
Frédéric Dubouchet <frederic.dubouchet@gmail.com>
2020-10-22 09:08:06 +0200

    push the asio chat version

--- client/client.cpp
+++ client/client.cpp
@@ -1,42 +1,167 @@
+//
+// chat_client.cpp
+// ~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2020 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <cstdlib>
+#include <deque>
 #include <iostream>
+#include <thread>
 #include <asio.hpp>
+#include "../common/message.h"
 
-int main(int ac, char** av)
+using asio::ip::tcp;
+
+typedef std::deque<chat_message> chat_message_queue;
+
+class chat_client
 {
-	asio::error_code ec;
-	asio::io_context context;
-	asio::ip::tcp::endpoint ep(asio::ip::make_address("208.97.177.124", ec), 80);
-	asio::ip::tcp::socket socket(context);
-	socket.connect(ep, ec);
-	if (!ec)
+public:
+	chat_client(asio::io_context& io_context,
+		const tcp::resolver::results_type& endpoints)
+		: io_context_(io_context),
+		socket_(io_context)
 	{
-		std::cout << "Connected!" << std::endl;
-	}
-	else
-	{
-		std::cout << "Failed to connect to address: " << ec.message() << std::endl;
-		return -1;
+		do_connect(endpoints);
 	}
 
-	if (socket.is_open())
+	void write(const chat_message& msg)
 	{
-		std::string request =
-			"GET /index.html HTTP/1.1\r\n"
-			"Host: example.com\r\n"
-			"Connection: close\r\n\r\n";
+		asio::post(io_context_,
+			[this, msg]()
+			{
+				bool write_in_progress = !write_msgs_.empty();
+				write_msgs_.push_back(msg);
+				if (!write_in_progress)
+				{
+					do_write();
+				}
+			});
+	}
 
-		socket.write_some(asio::buffer(request.data(), request.size()), ec);
-		socket.wait(socket.wait_read);
-		size_t bytes = socket.available();
-		std::cout << "Bytes Available: " << bytes << std::endl;
+	void close()
+	{
+		asio::post(io_context_, [this]() { socket_.close(); });
+	}
 
-		if (bytes > 0)
+private:
+	void do_connect(const tcp::resolver::results_type& endpoints)
+	{
+		asio::async_connect(socket_, endpoints,
+			[this](asio::error_code ec, tcp::endpoint)
+			{
+				if (!ec)
+				{
+					do_read_header();
+				}
+			});
+	}
+
+	void do_read_header()
+	{
+		asio::async_read(socket_,
+			asio::buffer(read_msg_.data(), chat_message::header_length),
+			[this](asio::error_code ec, std::size_t /*length*/)
+			{
+				if (!ec && read_msg_.decode_header())
+				{
+					do_read_body();
+				}
+				else
+				{
+					socket_.close();
+				}
+			});
+	}
+
+	void do_read_body()
+	{
+		asio::async_read(socket_,
+			asio::buffer(read_msg_.body(), read_msg_.body_length()),
+			[this](asio::error_code ec, std::size_t /*length*/)
+			{
+				if (!ec)
+				{
+					std::cout.write(read_msg_.body(), read_msg_.body_length());
+					std::cout << "\n";
+					do_read_header();
+				}
+				else
+				{
+					socket_.close();
+				}
+			});
+	}
+
+	void do_write()
+	{
+		asio::async_write(socket_,
+			asio::buffer(write_msgs_.front().data(),
+				write_msgs_.front().length()),
+			[this](asio::error_code ec, std::size_t /*length*/)
+			{
+				if (!ec)
+				{
+					write_msgs_.pop_front();
+					if (!write_msgs_.empty())
+					{
+						do_write();
+					}
+				}
+				else
+				{
+					socket_.close();
+				}
+			});
+	}
+
+private:
+	asio::io_context& io_context_;
+	tcp::socket socket_;
+	chat_message read_msg_;
+	chat_message_queue write_msgs_;
+};
+
+int main(int argc, char* argv[])
+{
+	try
+	{
+		if (argc != 3)
 		{
-			std::vector<char> buffer(bytes);
-			socket.read_some(asio::buffer(buffer.data(), buffer.size()), ec);
-			for (const auto& c : buffer)
-				std::cout << c;
+			std::cerr << "Usage: chat_client <host> <port>\n";
+			return 1;
 		}
+
+		asio::io_context io_context;
+
+		tcp::resolver resolver(io_context);
+		auto endpoints = resolver.resolve(argv[1], argv[2]);
+		chat_client c(io_context, endpoints);
+
+		std::thread t([&io_context]() { io_context.run(); });
+
+		char line[chat_message::max_body_length + 1];
+		while (std::cin.getline(line, chat_message::max_body_length + 1))
+		{
+			chat_message msg;
+			msg.body_length(std::strlen(line));
+			std::memcpy(msg.body(), line, msg.body_length());
+			msg.encode_header();
+			c.write(msg);
+		}
+
+		c.close();
+		t.join();
 	}
+	catch (std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
+
 	return 0;
 }
